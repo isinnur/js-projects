32ebcb7d023653d1ea27a2919f984b0054f2c97d
isinnur <isinngnyy@gmail.com>
2023-07-15 11:07:47 +0300

    quiz app

---
+++ quizApp/quiz.js
@@ -0,0 +1,61 @@
+const quiz = [
+    {
+        question: "1.Which language runs in a web browser?",
+        selected: null,
+        answers: [
+            { text: "java", correct: false },
+            { text: "C", correct: false },
+            { text: "Python", correct: false },
+            { text: "JavaScript", correct: true },
+        ]
+    },
+
+
+    {
+        question: "2.What does CSS stand for?",
+        selected: null,
+        answers: [
+            { text: "Central Style Sheets", correct: false },
+            { text: "Cascading Style Sheets", correct: true },
+            { text: "Cascading Simple Sheets", correct: false },
+            { text: "Cars SUVs Sailboats", correct: false },
+        ]
+    },
+
+    {
+        question: "3.What does HTML stand for?",
+        answers: [
+            { text: "Hypertext Markup Language", correct: true },
+            { text: "Hypertext Markdown Language", correct: false },
+            { text: "Hyperloop Machine Language", correct: false },
+            { text: "Helicopters Terminals Motorboats Lamborginis", correct: false },
+        ]
+    },
+
+
+    {
+        question: "4.What year was JavaScript launched?",
+        selected: null,
+        answers: [
+            { text: "1996", correct: false },
+            { text: "1995", correct: true },
+            { text: "1994", correct: false },
+            { text: "none of the above", correct: false },
+        ]
+    },
+
+
+    {
+        question: "5.Which built-in method returns the index within the calling String object of the first occurrence of the specified value?",
+        selected: null,
+        answers: [
+            { text: "getIndex()", correct: false },
+            { text: "location()", correct: false },
+            { text: "indexOf()", correct: true },
+            { text: "None of the above.", correct: false },
+        ]
+    },
+
+
+
+];
---
+++ quizApp/script.js
@@ -0,0 +1,193 @@
+const questionElement = document.getElementById('question');
+const answer_buttons = document.getElementById('answer-buttons');
+const next_button = document.getElementById('next-btn');
+const start_btn = document.querySelector('.start_btn');
+const app = document.querySelector('.app');
+const quizC = document.querySelector('.quiz');
+const result_page = document.querySelector('.result');
+const your_answers = document.querySelector('.your-answers');
+const answer_question = document.querySelector('.question');
+
+let currentIndex = 0;
+let score = 0;
+const selectedAnswers = [];
+
+
+const startQuiz = () => {
+    start_btn.addEventListener('click', () => {
+        app.classList.remove('show');
+        start_btn.classList.add('show');
+        showQuestion();
+    })
+}
+
+function showQuestion() {
+    resetsState();
+    let currentQuestion = quiz[currentIndex];
+    let questionNo = currentIndex + 1;
+    //soruyu getirir
+    questionElement.innerHTML = currentQuestion.question;
+
+    //seçenekleri getirir
+    currentQuestion.answers.forEach((answer) => {
+        const button = document.createElement('button');
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answer_buttons.appendChild(button);
+        if (answer.correct) {
+            //ifade true ise dataya true atanıyor
+            button.dataset.correct = answer.correct;
+        }
+
+
+        button.addEventListener('click', selectAnswer);
+
+
+    })
+
+}
+
+function resetsState() {
+    next_button.style.display = "none";
+    //yeni sorular gelmeden container'ın boşluğunu kontrol eder
+    while (answer_buttons.firstChild) {
+        answer_buttons.removeChild(answer_buttons.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    //seçtiğimiz seçenek 
+    const selectBtn = e.target;
+
+
+
+
+
+    //işaretlediğimiz seçenekleri en sonunda döstermek için
+    const selectedOption = selectBtn.innerHTML;
+    const currentQuestion = quiz[currentIndex];
+
+    //seçeneği diziye ekler
+    selectedAnswers[currentIndex] = selectedOption;
+
+    //seçtiğimiz seçeneğin doğru veya yanlışlığını kontrol eder
+    const isCorrect = selectBtn.dataset.correct === 'true';
+
+    //true ise classlisti correct değilse incorrect
+    if (isCorrect) {
+        selectBtn.classList.add('correct');
+        score++;
+    }
+    else {
+        selectBtn.classList.add('incorrect');
+    }
+
+    //yanlış seçeneği seçse de doğru seçeneği gösterir
+    Array.from(answer_buttons.children).forEach(button => {
+        if (button.dataset.correct === 'true') {
+            button.classList.add('correct');
+        }
+        button.disabled = true;
+    })
+    next_button.style.display = "block";
+}
+
+
+
+
+
+function resultPage() {
+    app.classList.add('show');
+    result_page.classList.remove('show');
+    result_page.innerHTML = `
+        <h2>Your score : ${score}/${quiz.length} </h2>
+        <div class="buttons">
+            <button class="restart">Restart</button>
+            <button class="results-btn">Results</button>
+        </div>
+    
+    `;
+
+    const restart = document.querySelector('.restart');
+    const results_btn = document.querySelector('.results-btn')
+    restart.addEventListener('click', () => {
+        location.reload();
+    })
+
+    results_btn.addEventListener('click', () => {
+        showResults();
+    })
+}
+
+function nextButtonHandler() {
+    currentIndex++;
+    if (currentIndex < quiz.length) {
+        showQuestion();
+    }
+
+    else {
+        resultPage();
+    }
+}
+
+next_button.addEventListener('click', () => {
+    if (currentIndex < quiz.length) {
+        nextButtonHandler();
+    }
+
+    else {
+        startQuiz();
+    }
+})
+
+function showResults() {
+    result_page.classList.add('show');
+    your_answers.classList.remove('show');
+
+    const resultsContainer = document.createElement('div');
+
+    quiz.forEach((question, index) => {
+        const questionContainer = document.createElement('div');
+        questionContainer.classList.add('question-container');
+
+        const questionElement = document.createElement('h2');
+        questionElement.innerHTML = question.question;
+        questionContainer.appendChild(questionElement);
+
+        const answerContainer = document.createElement('div');
+        answerContainer.classList.add('answer-container');
+
+        question.answers.forEach((answer) => {
+            const answerElement = document.createElement('button');
+            answerElement.innerHTML = answer.text;
+            answerElement.classList.add('answerEl');
+
+            if (answer.correct) {
+                answerElement.classList.add('correct');
+            }
+            if (selectedAnswers[index] === answer.text && !answer.correct) {
+                answerElement.classList.add('incorrect');
+            }
+
+            answerContainer.appendChild(answerElement);
+        });
+
+        questionContainer.appendChild(answerContainer);
+        resultsContainer.appendChild(questionContainer);
+    });
+
+    your_answers.innerHTML = '';
+    your_answers.appendChild(resultsContainer);
+
+    const restartButton = document.createElement('button');
+    restartButton.innerHTML = 'Restart';
+    restartButton.classList.add('restart');
+    result_page.appendChild(restartButton);
+    restartButton.addEventListener('click', () => {
+        location.reload();
+    });
+
+    your_answers.appendChild(restartButton);
+}
+
+startQuiz();
